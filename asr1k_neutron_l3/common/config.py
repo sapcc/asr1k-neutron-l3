a1c8ed3ab08e7b456696984ecf46362c7cc8664d
Andrew Karpow <andrew.karpow@sap.com>
2020-05-06 17:30:58 +0200

    fix config initalization

--- asr1k_neutron_l3/common/config.py
+++ asr1k_neutron_l3/common/config.py
@@ -17,6 +17,7 @@
 from oslo_log import log as logging
 from oslo_config import cfg
 from neutron.conf.agent import common
+from neutron.conf import service
 from neutron_lib._i18n import _
 
 LOG = logging.getLogger(__name__)
@@ -153,5 +154,6 @@ def register_l2_opts():
     cfg.CONF.register_opts(AGENT_STATE_OPTS, 'AGENT')
     cfg.CONF.register_opts(ASR1K_OPTS, "asr1k")
     cfg.CONF.register_opts(ASR1K_L2_OPTS, "asr1k_l2")
+    cfg.CONF.register_opts(service.RPC_EXTRA_OPTS)
 
     cfg.CONF.asr1k.yang_connection_pool_size = cfg.CONF.asr1k_l2.yang_connection_pool_size
