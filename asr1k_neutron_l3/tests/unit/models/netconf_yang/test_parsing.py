2c2da8be409e04b5ad7025142df66420bbda68b1
Sebastian Lohff <sebastian.lohff@sap.com>
2020-08-19 18:10:27 +0200

    Fix route-target import/export for 17.3 yang models

--- asr1k_neutron_l3/tests/unit/models/netconf_yang/test_parsing.py
+++ asr1k_neutron_l3/tests/unit/models/netconf_yang/test_parsing.py
@@ -73,3 +73,135 @@ class ParsingTest(base.BaseTestCase):
         self.assertEqual(bd.if_members[1].interface, "Port-channel2")
         bdif2_ids = [int(si.id) for si in bd.if_members[1].service_instances]
         self.assertEqual(bdif2_ids, [6379, 6383])
+
+    def test_vrf_parsing_with_rt(self):
+        vrf_xml_1609_multi = """
+<rpc-reply message-id="urn:uuid:68f753a2-4d41-45ad-98f7-3a6460c8dd9d" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <data>
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+      <vrf>
+        <definition>
+          <name>1ccc4863d8bc4c82affa0cb198e8d5d1</name>
+          <description>Router 1ccc4863-d8bc-4c82-affa-0cb198e8d5d1</description>
+          <rd>65148:871</rd>
+          <address-family>
+            <ipv4>
+              <export>
+                <map>exp-1ccc4863d8bc4c82affa0cb198e8d5d1</map>
+              </export>
+              <route-target>
+                <export>
+                  <asn-ip>4268359684:12345</asn-ip>
+                </export>
+                <export>
+                  <asn-ip>65148:12345</asn-ip>
+                </export>
+                <import>
+                  <asn-ip>4268359685:12345</asn-ip>
+                </import>
+                <import>
+                  <asn-ip>65148:12345</asn-ip>
+                </import>
+              </route-target>
+            </ipv4>
+          </address-family>
+        </definition>
+      </vrf>
+    </native>
+  </data>
+</rpc-reply>
+"""
+        vrf_xml_1609_single = """
+<rpc-reply message-id="urn:uuid:68f753a2-4d41-45ad-98f7-3a6460c8dd9d" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <data>
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+      <vrf>
+        <definition>
+          <name>1ccc4863d8bc4c82affa0cb198e8d5d1</name>
+          <description>Router 1ccc4863-d8bc-4c82-affa-0cb198e8d5d1</description>
+          <rd>65148:871</rd>
+          <address-family>
+            <ipv4>
+              <export>
+                <map>exp-1ccc4863d8bc4c82affa0cb198e8d5d1</map>
+              </export>
+              <route-target>
+                <export>
+                  <asn-ip>4268359684:12345</asn-ip>
+                </export>
+                <import>
+                  <asn-ip>4268359685:12345</asn-ip>
+                </import>
+              </route-target>
+            </ipv4>
+          </address-family>
+        </definition>
+      </vrf>
+    </native>
+  </data>
+</rpc-reply>
+"""
+
+        vrf_xml_1731 = """
+<rpc-reply message-id="urn:uuid:d40a38bd-0a67-4f09-86b1-42ea770a8b5e" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <data>
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+      <vrf>
+        <definition>
+          <name>1ccc4863d8bc4c82affa0cb198e8d5d1</name>
+          <description>Router 1ccc4863-d8bc-4c82-affa-0cb198e8d5d1</description>
+          <rd>65148:871</rd>
+          <address-family>
+            <ipv4>
+              <export>
+                <map>exp-1ccc4863d8bc4c82affa0cb198e8d5d1</map>
+              </export>
+              <route-target>
+                <export-route-target>
+                  <without-stitching>
+                    <asn-ip>65130.4:12345</asn-ip>
+                  </without-stitching>
+                  <without-stitching>
+                    <asn-ip>65148:12345</asn-ip>
+                  </without-stitching>
+                </export-route-target>
+                <import-route-target>
+                  <without-stitching>
+                    <asn-ip>65130.5:12345</asn-ip>
+                  </without-stitching>
+                  <without-stitching>
+                    <asn-ip>65148:12345</asn-ip>
+                  </without-stitching>
+                </import-route-target>
+              </route-target>
+            </ipv4>
+          </address-family>
+        </definition>
+      </vrf>
+    </native>
+  </data>
+</rpc-reply>
+"""
+
+        expected_single = {
+            "rt_export": {"4268359684:12345", },
+            "rt_import": {"4268359685:12345"},
+        }
+        expected_multi = {
+            "rt_export": {"4268359684:12345", "65148:12345"},
+            "rt_import": {"4268359685:12345", "65148:12345"},
+        }
+
+        context_1609 = FakeASR1KContext(version_min_1612=False)
+        context_1612 = FakeASR1KContext(version_min_1612=True)
+
+        for context, vrf_xml, expected in ((context_1609, vrf_xml_1609_single, expected_single),
+                                           (context_1609, vrf_xml_1609_multi, expected_multi),
+                                           (context_1612, vrf_xml_1731, expected_multi)):
+            vrf = VrfDefinition.from_xml(vrf_xml, context)
+            self.assertEqual(expected['rt_export'],
+                             set([rt.normalized_asn_ip for rt in vrf.address_family_ipv4.rt_export]),
+                             "rt_export failed for 1612={}".format(context.version_min_1612))
+            self.assertEqual(expected['rt_import'],
+                             set([rt.normalized_asn_ip for rt in vrf.address_family_ipv4.rt_import]),
+                             "rt_import failed for 1612={}".format(context.version_min_1612))
