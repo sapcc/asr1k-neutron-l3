f479c3ad009f1cf5773d2270ccd9fbdb9349fbc0
Sebastian Lohff <sebastian.lohff@sap.com>
2024-10-30 13:48:55 +0100

    Readd @classmethod decorator to get_for_vrf()
    Commit be552792eed2acc124cb509f3b7f805993b716fd readded get_for_vrf()
    but forgot to add the @classmethod decorator that is required by the
    method to work.

--- asr1k_neutron_l3/models/netconf_yang/nat.py
+++ asr1k_neutron_l3/models/netconf_yang/nat.py
@@ -268,6 +268,7 @@ class InterfaceDynamicNat(DynamicNat):
 
     VRF_XPATH_FILTER = "/native/ip/nat/inside/source/list-interface/list[id='NAT-{vrf}']"
 
+    @classmethod
     def get_for_vrf(cls, context, vrf=None):
         return cls._get_all(context=context, xpath_filter=cls.VRF_XPATH_FILTER.format(vrf=vrf))
 
