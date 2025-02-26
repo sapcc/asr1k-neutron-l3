sapcc/asr1k-neutron-l3

commit c6260656ba41d94842419e656522b4ba57a9f294

Migrate to new prefix list yang model
Cisco migrated their previously hierarchical way of managing prefix
lists (one entry per prefix list, then a list of entries per list) to a
model, where every entry is equal to each other, but they're now keyed
by name and no. As this new option is available on 17.6 and the old
model was deprecated and removed with 17.15, we now need to migrate to
the new model.

The new style brings with it some challenges, as we normally handle
prefix lists on a name-basis, but now we need to take care of them by
no/name. To keep the old behavior inside the driver, we consolidate the
seqs into a single prefix list when the config is pulled down from the
device.

We also have a problem with "getting a single item" vs "getting multiple
items", as the methods we're using have different needs here. As all
NyBase.get() methods filter for a single prefix list name, we return
only a single item in all cases where we only find one element.
Otherwise we return multiple items, which for example get_all_stubs()
can handle without problems. It is a bit confusing and if we ever rework
how the driver handles single/multiple instances of a class this would
be one of the first points to start refactoring.

asr1k_neutron_l3/models/netconf_yang/prefix.py
@@ -14,15 +14,18 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from collections import OrderedDict
+from oslo_log import log as logging
 
 from asr1k_neutron_l3.models.netconf_yang.ny_base import execute_on_pair, NC_OPERATION, NyBase
+from asr1k_neutron_l3.models.netconf_yang import xml_utils
 from asr1k_neutron_l3.common import utils
+
+LOG = logging.getLogger(__name__)
 
 
 class PrefixConstants(object):
     IP = 'ip'
-    PREFIX_LIST = 'prefix-list'
+    PREFIX_LISTS = 'prefix-lists'
     PREFIXES = 'prefixes'
     NAME = 'name'
     DESCRIPTION = 'description'
@@ -39,11 +42,11 @@ class Prefix(NyBase):
     ID_FILTER = """
       <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
         <ip>
-          <prefix-list>
+          <prefix-lists>
             <prefixes>
               <name>{id}</name>
             </prefixes>
-          </prefix-list>
+          </prefix-lists>
         </ip>
       </native>
     """
@@ -51,17 +54,18 @@ class Prefix(NyBase):
     GET_ALL_STUB = """
       <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
         <ip>
-          <prefix-list>
+          <prefix-lists>
             <prefixes>
               <name/>
+              <no/>
             </prefixes>
-          </prefix-list>
+          </prefix-lists>
         </ip>
       </native>
     """
 
-    LIST_KEY = PrefixConstants.PREFIX_LIST
-    ITEM_KEY = PrefixConstants.PREFIXES
+    LIST_KEY = PrefixConstants.IP
+    ITEM_KEY = PrefixConstants.PREFIX_LISTS
 
     @classmethod
     def __parameters__(cls):
@@ -73,22 +77,39 @@ class Prefix(NyBase):
 
     @classmethod
     def remove_wrapper(cls, dict, context):
-        dict = super(Prefix, cls)._remove_base_wrapper(dict, context)
+        dict = super().remove_wrapper(dict, context)
         if dict is None:
             return
-        dict = dict.get(PrefixConstants.IP, dict)
-        dict = dict.get(cls.LIST_KEY, dict)
+        dict = dict.get(cls.ITEM_KEY, {})
+        if PrefixConstants.PREFIXES not in dict:
+            return
 
-        return dict
+        # merge together prefix lists with the same name
+        prefixes = dict[PrefixConstants.PREFIXES]
+        if not isinstance(prefixes, list):
+            prefixes = [prefixes]
 
-    def _wrapper_preamble(self, dict, context):
-        result = {}
-        result[self.LIST_KEY] = dict
-        result = {PrefixConstants.IP: result}
+        seqs = {}
+        for prefix in prefixes:
+            pfx_name = prefix.get(PrefixConstants.NAME)
+            seqs.setdefault(pfx_name, []).append(prefix)
+
+        result = [
+            {
+                PrefixConstants.NAME: name,
+                PrefixConstants.SEQ: seq,
+            }
+            for name, seq in seqs.items()
+        ]
+
+        # default get will only have a single entry due to the xml filter
+        # get_all_stubs_from_device() will have a single or multiple entries (and can handle both)
+        # --> we unpack the list in case of a single entry
+        if len(result) == 1:
+            result = result[0]
+
+        result = {cls.ITEM_KEY: result}
         return result
-
-    def __init__(self, **kwargs):
-        super(Prefix, self).__init__(**kwargs)
 
     @property
     def neutron_router_id(self):
@@ -107,30 +128,41 @@ class Prefix(NyBase):
     @execute_on_pair()
     def update(self, context):
         if len(self.seq) > 0:
-            return super(Prefix, self)._update(context=context, method=NC_OPERATION.PUT)
+            return super(Prefix, self)._update(context=context)
         else:
             return super(Prefix, self)._delete(context=context)
 
+    def preflight(self, context):
+        # delete all rules that should not be on the device (by seq no, rest is done by update-replace)
+        # NOTE: if this deletion succeeds and a later update fails we might get in a situation
+        #       where a route is deleted here (with seq 30) that cannot be readded (with seq 20)
+        #       and we loose an entry until the next update. The probability is pretty low, but we
+        #       should keep this in mind if we see behavior like that
+        dev_pfx_list = self._internal_get(context=context)
+        if dev_pfx_list and dev_pfx_list.seq:
+            dev_seq_to_remove = {seq.no for seq in dev_pfx_list.seq} - {seq.no for seq in self.seq}
+            if dev_seq_to_remove:
+                LOG.warning("Prefix-list %s needs cleaning - seq %s present on device but not in neutron",
+                            self.name, ", ".join(map(str, dev_seq_to_remove)))
+                dev_pfx_list.seq = dev_seq_to_remove
+                dev_pfx_list._delete(context=context)
+
     def to_dict(self, context):
-        prefix = OrderedDict()
-        prefix[PrefixConstants.NAME] = self.name
-        prefix[PrefixConstants.SEQ] = []
-
+        prefixes = []
         for seq in self.seq:
-            prefix[PrefixConstants.SEQ].append(seq.to_dict(context))
-
-        result = OrderedDict()
-        result[PrefixConstants.PREFIXES] = prefix
-
-        return dict(result)
+            seq_dict = {PrefixConstants.NAME: self.name}
+            seq_dict.update(seq.to_dict(context))
+            prefixes.append(seq_dict)
+        return {PrefixConstants.PREFIX_LISTS: {PrefixConstants.PREFIXES: prefixes}}
 
     def to_delete_dict(self, context):
-        prefix = OrderedDict()
-        prefix[PrefixConstants.NAME] = self.name
-        result = OrderedDict()
-        result[PrefixConstants.PREFIXES] = prefix
-
-        return dict(result)
+        prefixes = []
+        for seq in self.seq:
+            prefixes.append({
+                PrefixConstants.NAME: self.name,
+                PrefixConstants.NUMBER: seq.no,
+            })
+        return {PrefixConstants.PREFIX_LISTS: {PrefixConstants.PREFIXES: prefixes}}
 
 
 class PrefixSeq(NyBase):
@@ -138,12 +170,6 @@ class PrefixSeq(NyBase):
     def __parameters__(cls):
         return [
             {'key': 'no', 'id': True},
-            {'key': 'deny_ip', 'yang-key': 'ip', 'yang-path': 'deny'},
-            {'key': 'deny_ge', 'yang-key': 'ge', 'yang-path': 'deny'},
-            {'key': 'deny_le', 'yang-key': 'le', 'yang-path': 'deny'},
-            {'key': 'permit_ip', 'yang-key': 'ip', 'yang-path': 'permit'},
-            {'key': 'permit_ge', 'yang-key': 'ge', 'yang-path': 'permit'},
-            {'key': 'permit_le', 'yang-key': 'le', 'yang-path': 'permit'},
 
             # new seq format for 17.3+
             {'key': 'action'},
@@ -152,40 +178,16 @@ class PrefixSeq(NyBase):
             {'key': 'ge'},
         ]
 
-    def __init__(self, **kwargs):
-        super(PrefixSeq, self).__init__(**kwargs)
-
-        if self.deny_ip is not None and self.permit_ip is not None:
-            raise Exception("Permit and Deny statements canot coexist on the same sequence")
-
     def to_dict(self, context):
-        seq = OrderedDict()
+        seq = {}
 
+        seq[xml_utils.OPERATION] = NC_OPERATION.PUT
         seq[PrefixConstants.NUMBER] = self.no
-        if self.action is not None:
-            # parameters passed in 17.3 format
-            seq[PrefixConstants.ACTION] = self.action
-            seq[PrefixConstants.IP] = self.action_ip
-            if self.ge:
-                seq[PrefixConstants.GE] = self.ge
-            if self.le:
-                seq[PrefixConstants.GE] = self.le
-        else:
-            # parameters passed in old format by the driver
-            if self.deny_ip is not None:
-                seq[PrefixConstants.ACTION] = PrefixConstants.DENY
-                seq[PrefixConstants.IP] = self.deny_ip
-                if self.deny_ge is not None:
-                    seq[PrefixConstants.GE] = self.deny_ge
-                if self.deny_le is not None:
-                    seq[PrefixConstants.LE] = self.deny_le
-
-            if self.permit_ip is not None:
-                seq[PrefixConstants.ACTION] = PrefixConstants.PERMIT
-                seq[PrefixConstants.IP] = self.permit_ip
-                if self.permit_ge is not None:
-                    seq[PrefixConstants.GE] = self.permit_ge
-                if self.permit_le is not None:
-                    seq[PrefixConstants.LE] = self.permit_le
+        seq[PrefixConstants.ACTION] = self.action
+        seq[PrefixConstants.IP] = self.action_ip
+        if self.ge:
+            seq[PrefixConstants.GE] = self.ge
+        if self.le:
+            seq[PrefixConstants.GE] = self.le
 
         return seq

asr1k_neutron_l3/models/neutron/l3/prefix.py
@@ -44,7 +44,8 @@ class ExtPrefix(BasePrefix):
             i = 1
             for subnet in sorted(self.gateway_interface.subnets, key=itemgetter('id')):
                 self.has_prefixes = True
-                self._rest_definition.add_seq(prefix.PrefixSeq(no=i * 10, permit_ip=subnet.get('cidr')))
+                self._rest_definition.add_seq(
+                    prefix.PrefixSeq(no=i * 10, action="permit", action_ip=subnet.get('cidr')))
                 i += 1
 
 
@@ -57,7 +58,8 @@ class SnatPrefix(BasePrefix):
         for interface in sorted(self.internal_interfaces, key=attrgetter('id')):
             for subnet in sorted(interface.subnets, key=itemgetter('id')):
                 self.has_prefixes = True
-                self._rest_definition.add_seq(prefix.PrefixSeq(no=i * 10, permit_ip=subnet.get('cidr')))
+                self._rest_definition.add_seq(
+                    prefix.PrefixSeq(no=i * 10, action="permit", action_ip=subnet.get('cidr')))
                 i += 1
 
 
@@ -72,5 +74,6 @@ class RoutePrefix(BasePrefix):
                 self.has_prefixes = True
                 cidr = subnet.get('cidr')
                 permit_ge = utils.prefix_from_cidr(cidr) + 1
-                self._rest_definition.add_seq(prefix.PrefixSeq(no=i * 10, permit_ip=cidr, permit_ge=permit_ge))
+                self._rest_definition.add_seq(
+                    prefix.PrefixSeq(no=i * 10, action="permit", action_ip=cidr, ge=permit_ge))
                 i += 1

commit 22a370cbf63f268a452ad2f06bdd9f42c03d123d

Only delete single prefix list, not all of them
With the transition to the new prefix list model we got another layer of
indirection. The NyBase class by default handles deletes by adding the
operation="delete" to the xml tag it considers being the "main" tag for
the object. Due to Cisco removing the hierarchical way of handling
prefix lists and putting everything into one list, this outermost tag
now is the one holding all entries of all prefix lists. A delete
operation on that tag results in all entries being deleted.

Here is an example for a config request that looks like it would delete
one ipv6 entry, but in fact deletes all of them:
  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
    <ipv6>
      <prefix-lists operation="delete">
        <prefixes>
          <name>ext-1bc9cf3742c04f86a2d01ebe513e3287</name>
          <no>10</no>
        </prefixes>
      </prefix-lists>
    </ipv6>
  </native>
</config>

To fix this we need to move the operation away from the <prefix-lists>
tag into the <prefixes> tag, like this:
<config>
  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
    <ipv6>
      <prefix-lists>
        <prefixes operation="remove">
          <name>ext-1bc9cf3742c04f86a2d01ebe513e3287</name>
          <no>10</no>
        </prefixes>
      </prefix-lists>
    </ipv6>
  </native>
</config>

We also switch the operation from "delete" (delete the entry, throw an
error if it is not present) to "remove" (delete the entry, ignore if it
is not present).

This bug was introduced in c626065 when migrating on the new prefix list
model.

asr1k_neutron_l3/models/netconf_yang/prefix.py
@@ -153,6 +153,11 @@ class PrefixBase(NyBase):
         else:
             return super()._delete(context=context)
 
+    def _delete_no_retry(self, context, method=NC_OPERATION.DELETE, postflight=True):
+        # override method - we want to have a patch, as due to the new prefix model the delete operation
+        #                   needs to be one level deeper in the xml request (see to_delete_dict())
+        super()._delete_no_retry(context, method=NC_OPERATION.PATCH, postflight=postflight)
+
     def preflight(self, context):
         # delete all rules that should not be on the device (by seq no, rest is done by update-replace)
         # NOTE: if this deletion succeeds and a later update fails we might get in a situation
@@ -180,6 +185,7 @@ class PrefixBase(NyBase):
         prefixes = []
         for seq in self.seq:
             prefixes.append({
+                xml_utils.OPERATION: NC_OPERATION.REMOVE,
                 PrefixConstants.NAME: self.name,
                 PrefixConstants.NUMBER: seq.no,
             })